0a659dccf647ed0ba7c71f6b9fc76256d45f97f1
Arditto Trianggada <atrianggada@gmail.com>
2018-01-04 13:18:18 +0700

    Update command.sh

--- kafka-connect/hdfs-sink/command.sh
+++ kafka-connect/hdfs-sink/command.sh
@@ -1,1 +1,4 @@
 ## Sink data to hdfs
+cp /opt/confluent/etc/kafka-connect-hdfs/quickstart-hdfs.properties /opt/confluent/etc/kafka-connect-hdfs/sink-test.properties
+confluent load hdfs-sink -d /opt/confluent/etc/kafka-connect-hdfs/sink-test.properties
+confluent log connect
